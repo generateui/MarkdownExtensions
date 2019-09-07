37cea66e2a0f7551af812f992c67245c288f9028
Ruud <rtimon@gmail.com>
2019-09-07 22:18:32 +0200

    test Folder

---
+++ MarkdownExtensions.UnitTest/FolderTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkdownExtensions.UnitTest
+{
+	[TestClass]
+	public class FolderTest
+	{
+		[TestMethod]
+		public void OneLevelRelativeFolder_HasCorrectFullPath()
+		{
+			var relativeFolder = new RelativeFolder(@"folder");
+			var absoluteFolder = new AbsoluteFolder(@"c:\");
+			var folder = new Folder(absoluteFolder, relativeFolder);
+			Assert.AreEqual(@"c:\folder", folder.Absolute.FullPath);
+		}
+
+		[TestMethod]
+		public void AbsoluteFolder_HasCorrectFullPath()
+		{
+			var absoluteFolder = new AbsoluteFolder(@"c:\folder");
+			var folder = new Folder(absoluteFolder);
+			Assert.AreEqual(@"c:\folder", folder.Absolute.FullPath);
+		}
+	}
+}
